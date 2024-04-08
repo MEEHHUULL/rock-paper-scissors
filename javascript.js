8c129ec009446b5e7a28612f0f3af0692ac96e2d
Mehul Shah <164782550+MEEHHUULL@users.noreply.github.com>
2024-04-08 18:28:49 +0100

    it works on a rudimentary level

--- javascript.js
+++ javascript.js
@@ -1,33 +1,41 @@
+// computer turn
 compchoice = ['rock', 'paper', 'scissors']
 function getComputerChoice(compchoice) {
     let compselection = compchoice[Math.floor(Math.random()*compchoice.length)];
     return compselection
 }
 
-// okay, so this gives a random output out of rock paper scissors for the computer
-// i did this by making an array with r/p/s 
-// next i took a random number between 0-1 but scaled by the length of my array so actually between 0-3
-// next i needed this to make an integer so i used math.floor to round down to the nearest integer 0/1/2 as my array consists of r/p/s
-// not bad at all, so far so good. 
 
 
-// next i need to allow the human to enter a r/p/s value. 
-// i guess i also need to worry about how the human will type this in .. Rock, rock, ROCK.. 
-// i guess i will need to convert the input all to either lower/uppercase 
-//then later i will need to compare human input vs computer input but let's not get ahead of ourselves. 
 
-function humanturn(){
-return prompt('Select Rock/Paper/Scissors').toLowerCase();
-}
+// human turn 
 
-//ok, so i think i've got the above done. it was actually pretty simple. i just used the prompt to allow the human to enter 
+const rock = document.querySelector("#rock");
+const paper = document.querySelector("#paper");
+const scissors = document.querySelector("#scissors");
+
+humanselection = "";
+ 
+rock.addEventListener("click", () => {
+    humanselection = "rock";
+    game();
+});
+
+paper.addEventListener("click", () => {
+    humanselection = "paper";
+    game();
+});
+
+scissors.addEventListener("click",() => {
+    humanselection = "scissors";
+    game();
+});
 
 
+// the actual game
 function game() {
 
-let compselection = getComputerChoice(compchoice); //edefines output from getComputerChoice as game function does not store this
-
-let humanselection = humanturn(); // redefines output from humanturn as game function does not store this
+    let compselection = getComputerChoice(compchoice); 
 
 let result = "";
 
@@ -53,10 +61,10 @@ else {
     result = 'you lose, try again!';
 }
 
-game()
+ const displayResult = document.createElement("div"); 
+ displayResult.classList.add("displayResult");
+ displayResult.textContent = result;
+ document.body.append(displayResult);
 
-// functions are all independent of each other. 
-// all a function does is return a value in a given data type 
-// you then need to define this returned value in other functions in order for it to be used by the other functions 
-// OR you can simply use the function name to give you the value 
-// but i prefer redefining the function.. i think it's continuity or something like that 
+}
+
